7e5724166879ec20a817f7353e1f76eefeeed8d4
Rick <richims026@gmail.com>
2025-04-03 15:59:31 -0500

    Upl informe.html

--- main.R
+++ main.R
@@ -11,6 +11,7 @@ library(terra)
 library(raster)
 library(sf)
 library(pacman)
+library(janitor)
 
 # .lintr
 text <- c(
@@ -37,5 +38,54 @@ file.create("notes.md")
 
 # Procesamiento
 
+## Join Enaho
+
+## Widgets Quarto
+
+## Formato Spss
+
+## Janitor
 
 options("width" = 10000)
+
+mpg %>% str()
+mpg %>% dim()
+mpg %<>% mutate(across(where(is.character), as.factor))
+
+mpg %>%
+  dplyr::select(drv, fl, trans) %>%
+  get_dupes()
+
+mpg %>% cross_cases(fl, drv)
+
+tmp <- sheet(x = 1:5, y = 11:15)
+names(tmp) <- c("Alpha Beta", "Gamma Epsilon")
+
+clean_names(tmp)
+
+tmp <-
+  mpg %>%
+  xtabs(~ drv + fl, .) %>%
+  prop.table() * 100
+
+tmp %>%
+  round(., 3) %>%
+  addmargins()
+
+## Maps Leaflet
+sede_op <- read_sf(file.choose())
+sede_op %>% str()
+
+sede_op %>%
+  leaflet() %>%
+  addProviderTiles(as.character(providers[152])) %>%
+  addPolygons(
+    popup = ~ paste0("<b>", SEDEOPERAT, "</b><hr>", CODSEDE),
+    color = "orange",
+    opacity = .7
+  )
+
+sede_op %>% filter(SEDEOPERAT == "HUANUCO")
+sede_op %>%
+  dplyr::select(SEDEOPERAT, CODSEDE) %>%
+  sheet()
